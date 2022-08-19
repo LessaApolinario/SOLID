2d8ff3c53c6fa0f3b35433d00ac1f4f71d6c4a83
Lessa Apolinario <lessaapolinario@gmail.com>
2022-08-19 10:48:41 -0300

    change if statement rule

--- 03-OCP/01-calculadora-precos/src/TabelaDePreco.ts
+++ 03-OCP/01-calculadora-precos/src/TabelaDePreco.ts
@@ -1,7 +1,7 @@
 export class TabelaDePreco {
   desconto(valor: number): number {
-    if (valor > 200) return 0.2
     if (valor > 500) return 0.5
+    else if (valor > 200) return 0.2
     return 0
   }
 }
